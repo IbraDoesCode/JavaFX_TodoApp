d89faad70658bc17e0f1a0224c9243f46189a0e4
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-08-30 12:02:05 +0800

    Method and variable updates
    - Updated todoList attribute name to todoData
    - Reverted back to using enhanced for loop in getPendingTodos().

--- src/service/TodoManager.java
+++ src/service/TodoManager.java
@@ -8,29 +8,37 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TodoManager {
 
     private final FileService fileService;
-    private final List<String[]> todoList;
+    private final List<String[]> todoData;
     private final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
 
     public TodoManager(FileService fileService) {
         this.fileService = fileService;
-        this.todoList = fileService.readCsv();
+        this.todoData = fileService.readCsv();
     }
 
     public String generateId() {
-        int todoNum = todoList.isEmpty() ? 1 : Integer.parseInt(todoList.getLast()[0].split("-")[1]) + 1;
+        int todoNum = todoData.isEmpty() ? 1 : Integer.parseInt(todoData.getLast()[0].split("-")[1]) + 1;
         return LocalDate.now().format(DateTimeFormatter.ofPattern("MM:dd:yy")) + "-" + todoNum;
     }
 
     public List<Todo> getPendingTodos() {
-        return todoList.stream()
-                .filter(row -> row[3].equals("PENDING")) // only get row with pending Status
-                .map(row -> new Todo(row[0], row[1], LocalDateTime.parse(row[2], DATE_FORMAT), Status.valueOf(row[3]), null)) // create new Todo Object
-                .toList(); // return as List
+        List<Todo> pendingTasks = new ArrayList<>();
+
+        for (String[] row : todoData) {
+            if (row[3].equals("PENDING")) {
+                pendingTasks.add(new Todo(row[0],
+                        row[1],
+                        LocalDateTime.parse(row[2], DATE_FORMAT),
+                        Status.valueOf(row[3]),
+                        null));
+            }
+        }
+
+        return pendingTasks;
     }
 
     public String[] convertTodoToArray(Todo todo) {
@@ -45,21 +53,21 @@ public class TodoManager {
 
     public void addTodo(Todo todo) {
         // add the new task to the in-memory data list
-        todoList.add(convertTodoToArray(todo));
+        todoData.add(convertTodoToArray(todo));
 
         // append the new task to the CSV
         fileService.appendData(convertTodoToArray(todo));
     }
 
     public void deleteTodo(String todo) {
-        todoList.removeIf(row -> row[1].equals(todo));
+        todoData.removeIf(row -> row[1].equals(todo));
 
         // write the updated data list back to the CSV
-        fileService.writeData(todoList);
+        fileService.writeData(todoData);
     }
 
     public void updateTodoStatus(String todo) {
-        for (String[] row : todoList) {
+        for (String[] row : todoData) {
             if (row[1].equals(todo)) {
                 // update status
                 row[3] = String.valueOf(Status.COMPLETE);
@@ -68,7 +76,7 @@ public class TodoManager {
             }
         }
         // Save the changes in the CSV
-        fileService.writeData(todoList);
+        fileService.writeData(todoData);
     }
 
 }
