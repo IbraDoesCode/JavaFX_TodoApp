b44c2f2fd62584ebf939160a25c70132aa4d6016
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-08-30 12:05:40 +0800

    Removed unused showAlert method.

--- src/util/AlertUtil.java
+++ src/util/AlertUtil.java
@@ -2,18 +2,11 @@ package util;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 
 import java.util.Optional;
 
 public class AlertUtil {
-
-    public static void showAlert(Alert.AlertType alertType, String title, String content) {
-        Alert alert = new Alert(alertType);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(content);
-        alert.showAndWait();
-    }
 
     public static boolean confirmAction(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
