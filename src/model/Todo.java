ad3183f6d20cb55ea1cc9378b80a16092abd1fca
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-08-30 12:05:10 +0800

    Made class variables final

--- src/model/Todo.java
+++ src/model/Todo.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 public class Todo {
 
-    private String id;
-    private LocalDateTime dateCreated;
-    private Status status;
-    private LocalDateTime dateCompleted;
-    private String todo;
+    private final String id;
+    private final LocalDateTime dateCreated;
+    private final Status status;
+    private final LocalDateTime dateCompleted;
+    private final String todo;
 
     public Todo(String id, String todo, LocalDateTime dateCreated, Status status, LocalDateTime dateCompleted) {
         this.id = id;
@@ -38,14 +38,4 @@ public class Todo {
         return dateCompleted;
     }
 
-    @Override
-    public String toString() {
-        return "Todo{" +
-                "id='" + id + '\'' +
-                ", todo='" + todo + '\'' +
-                ", dateCreated=" + dateCreated +
-                ", status=" + status +
-                ", dateCompleted=" + dateCompleted +
-                '}';
-    }
 }
