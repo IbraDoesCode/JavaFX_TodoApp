590e073c587879cc38bc529401d55676c3370576
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2024-08-30 19:50:23 +0800

    Convert Todo class to Todo record for simplicity.

--- src/model/Todo.java
+++ src/model/Todo.java
@@ -2,40 +2,6 @@ package model;
 
 import java.time.LocalDateTime;
 
-public class Todo {
-
-    private final String id;
-    private final LocalDateTime dateCreated;
-    private final Status status;
-    private final LocalDateTime dateCompleted;
-    private final String todo;
-
-    public Todo(String id, String todo, LocalDateTime dateCreated, Status status, LocalDateTime dateCompleted) {
-        this.id = id;
-        this.todo = todo;
-        this.dateCreated = dateCreated;
-        this.status = status;
-        this.dateCompleted = dateCompleted;
-    }
-
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTodo() {
-        return todo;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public LocalDateTime getDateCompleted() {
-        return dateCompleted;
-    }
+public record Todo(String id, String todo, LocalDateTime dateCreated, Status status, LocalDateTime dateCompleted) {
 
 }
